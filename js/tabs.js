5d2f18667b23b445c32d7c334c3f73837ed61535
Siddharth Singh <simsid66@gmail.com>
2021-11-08 01:14:50 +0530

    Complete prize css

--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,16 @@
 const revealTab = (evt, tabName) => {
-    Array.from(document.getElementsByClassName("tabcontent")).forEach(
+    Array.from(document.getElementsByClassName("tab-contents")).forEach(
         tab => (tab.style.display = "none")
     );
-    Array.from(document.getElementsByClassName("tablinks")).forEach(link =>
-        link.className.replace(" active", "")
+    Array.from(document.getElementsByClassName("tab")).forEach(link =>
+        link.classList.remove("active")
     );
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 };
 
 
-Array.from(document.getElementsByClassName("tablinks")).forEach(link =>
+Array.from(document.getElementsByClassName("tab")).forEach(link =>
     link.addEventListener("click", evt =>
         revealTab(evt, link.id.split("link-").at(-1))
     )
